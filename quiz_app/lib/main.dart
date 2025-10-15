46a754019879a19bd0a9380b975233d29089b794
elwessdev <benaliosama3@gmail.com>
2025-10-15 23:53:25 +0100

    supabase + enhance deisng

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
-import "Home.dart";
-import "Login.dart";
-import "Landing.dart";
+import "SplashScreen.dart";
+import "Auth/Signin.dart";
+import "Auth/Signup.dart";
+import 'Service/auth_gate.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'App/Game.dart';
 
-void main() {
+void main() async{
+  await Supabase.initialize(
+    url: 'https://hqwpikxeejsutniyhssm.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhxd3Bpa3hlZWpzdXRuaXloc3NtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0NzIzMzIsImV4cCI6MjA3NjA0ODMzMn0.oCYorJZ2RvJOv0Pc9_buI3dJ7sXy7hHHFNdWKkROiJU',
+  );
   runApp(const MyApp());
 }
+
+// #4e75ff
+// #0a1653
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,14 +23,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'wessQuizy',
+      title: 'wessQuizyy',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
+        primaryColor: Color(0xFF0a1653),
+        scaffoldBackgroundColor: Color.fromARGB(255, 255, 255, 255),
+        fontFamily: 'Roboto',
       ),
-      home: Scaffold(
-        body: LoginPage(),
-      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/authgate': (context) => const AuthGate(),
+        '/signin': (context) => const SignInPage(),
+        '/signup': (context) => const SignUpPage(),
+        '/game': (context) => const GamePage(),
+      },
     );
   }
 }
